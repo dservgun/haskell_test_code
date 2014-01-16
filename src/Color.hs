9cebbed64cc75d8c6ed62aa0e26d74c64ffd139d
Dinkar Ganti <dinkar.ganti@gmail.com>
2014-01-16 04:10:46 +0000

    Modeling color and point.  Not an ideal solution.

---
+++ src/Color.hs
@@ -0,0 +1,20 @@
+module Color where
+type Red = Int
+type Blue = Int
+type Green = Int
+type Cyan = Int
+type Magenta = Int
+type Yellow = Int
+type Key = Int -- Key and black are synonyms
+type Black = Int
+
+data Color = RGB (Red, Blue, Green)  | CMYK (Cyan, Magenta, Yellow, Key)
+-- This file attempts to implement the point hierarchy as defined in 
+-- ocaml manual.
+mod256 a b = (a + b) `mod` 256
+instance Num Color where
+    c1 + c2 = case (c1, c2) of
+                (RGB(a, b, c), RGB(x, y, z)) -> RGB (( mod256 a x), (mod256 b y), (mod256 c z))
+                (CMYK(a , b, c, d), CMYK(w, x, y, z)) -> CMYK((mod256 a w), (mod256 b x), (mod256 c y), (mod256 d z))
+                (_, _) -> error "Invalid type"
+                
--- src/Point.hs
+++ src/Point.hs
@@ -1,17 +1,5 @@
 module Point where
-
-type Red = Int
-type Blue = Int
-type Green = Int
-type Cyan = Int
-type Magenta = Int
-type Yellow = Int
-type Key = Int -- Key and black are synonyms
-type Black = Int
-
-data Color = RGB (Red, Blue, Green)  | CMYK (Cyan, Magenta, Yellow, Key)
--- This file attempts to implement the point hierarchy as defined in 
--- ocaml manual.
+import Color
 class Point a where
     getX :: a -> XAxis
     getY :: a -> YAxis
@@ -23,10 +11,16 @@ class (Point a) => ColoredPoint a where
 -- the axes are defined in terms of the primitive int
 -- though the Class Num is defined in terms of Integer allowing for arbitrary precision.
 -- fromIntegral converts an Integer to Int.
+-- As we can clearly see, perhaps there is a better way to represent inheritance?
+-- or is inheritance not worth the trouble supporting.
+
 type XAxis = Int
 type YAxis = Int
 data TwoDPoint = TwoDPoint XAxis YAxis
     deriving (Show, Ord, Read, Eq)
+    
+data TwoDColorPoint = TwoDColorPoint XAxis YAxis Color
+
 instance Num TwoDPoint where
     (TwoDPoint x1 y1) + (TwoDPoint x2 y2) = TwoDPoint (x1 + x2) (y1 + y2)
     (TwoDPoint x1 y1) - (TwoDPoint x2 y2) = TwoDPoint (x1 - x2)  (y1 - y2)
@@ -34,10 +28,19 @@ instance Num TwoDPoint where
     (TwoDPoint x1 y1) * (TwoDPoint x2 y2) = TwoDPoint (x1 * x2) (y1 * y2)
     signum x = x
     fromInteger i = TwoDPoint (fromIntegral i) (fromIntegral i)
-     
+
+instance Num TwoDColorPoint where
+    (TwoDColorPoint x1 y1 c1) + (TwoDColorPoint x2 y2 c2) = TwoDColorPoint (x1 + x2) (y1 + y2) (c1 + c2)
+    
 instance Point TwoDPoint where
     getX (TwoDPoint x _) = x
     getY (TwoDPoint _ y) = y
     move p1 p2 = p1 + p2
-    
+
+instance Point TwoDColorPoint where
+    getX (TwoDColorPoint x _ _) = x
+    getY (TwoDColorPoint _ y _) = y
+    move p1 p2 = p1 + p2    
+instance ColoredPoint TwoDColorPoint where
+    color (TwoDColorPoint _ _ c) = c
         
